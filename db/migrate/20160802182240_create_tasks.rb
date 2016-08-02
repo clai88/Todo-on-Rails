448df9b9bedc65494b7d2159fd82094747f3f6a5
Chris <clai88@gmail.com>
2016-08-02 14:24:57 -0400

    added models for list and task

---
+++ db/migrate/20160802182240_create_tasks.rb
@@ -0,0 +1,11 @@
+class CreateTasks < ActiveRecord::Migration
+  def change
+    create_table :tasks do |t|
+      t.string :name
+      t.integer :list_id
+      t.date :due_on
+
+      t.timestamps null: false
+    end
+  end
+end
